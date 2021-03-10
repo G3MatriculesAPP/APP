97c1b4784e86726c298b5f5f62c5c9ddcdcebbd4
unknown <nomengerard@gmail.com>
2021-03-10 20:06:06 +0100

    appTabs

--- www/js/init.js
+++ www/js/init.js
@@ -1,7 +1,35 @@
-(function($){
-  $(function(){
+// Arxiu init.js + index.js
 
+
+  // Or with jQuery
+
+   
+
+  (function($){
+    $(function(){
+   
+  
+    var options = { "swipeable": true };
+    var el = document.getElementById('tabs');
+    var instance = M.Tabs.init(el, options);
+    
     $('.sidenav').sidenav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+   
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+   
+  var bcerca = document.getElementById("bcerca");
+  
+  
+  
+  document.addEventListener('deviceready', onDeviceReady, false);
+   
+  function onDeviceReady() {
+      // Cordova is now initialized. Have fun!
+  
+      console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+      
+      //document.getElementById('deviceready').classList.add('ready');
+  }
+  
+  
