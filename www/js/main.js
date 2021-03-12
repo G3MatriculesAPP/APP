a5c7a44e1a5d7309f083767cd791b72f095392ae
Xavi-PL <xavipl.mvk25@gmail.com>
2021-03-12 15:31:25 +0100

    ADDED: Login CONN w/API

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,6 @@
 (function ($) {
     "use strict";
 
-
-    /*==================================================================
-    [ Focus input ]*/
     $('.input100').each(function(){
         $(this).on('blur', function(){
             if($(this).val().trim() != "") {
@@ -20,21 +17,34 @@
 var btnLogin = document.getElementById("btnLogin").onclick = checkLogin;
 
 function checkLogin(){
+
+    // checkLogin()
+    // Se comunica con la API y le pasa el valor introducido del usuario en itEmail
+    // y el valor encriptado en MD5 introducido en itPassword, si la API devuelve STATUS 200
+    // redirecciona al usuario a "dashboard.html" y guarda el TOKEN enviado por la API en LocalStorage
+    // si la API devuelve STATUS 500 manda una alerta al usuario comunicando que el email/contraseña
+    // son incorrectas.
     
     $.ajax({
         method: "POST",
-        url: "http://localhost:25012/alumnes/login",
+        // url: "http://localhost:5000/alum/login",               // [DEBUG] - Para pruebas LOCALHOST
+        url: "https://g3matriculesapp.herokuapp.com/alum/login",  // [DEBUG] - Para pruebas con HERKOU
         datatype: "json",
         data: ({
-          username: $("#itUsername").val(),
-          password: md5($("#itPassword").val())
+          itEmail: $("#itEmail").val(),
+          itPassword: md5($("#itPassword").val())
         }),
         success: function(result){
-
           if(result){
-            window.location = 'http://www.google.com/'
+            localStorage.setItem("TOKEN", result.token)
+            window.location = 'dashboard.html'    // [DEBUG] - Esto hay que cambiarlo por "index.html" y esta por "login.html"
+                                                  // hay que hacer que la APP abra primero el dashboard y si no tiene
+                                                  // login activo redirigir a esta.
           }
 
+        },
+        error: function(result){
+          alert('Usuario/Contraseña incorrecta...')
         }
       });
 
