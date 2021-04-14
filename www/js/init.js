3924c057d81a72a64d95f4d11c3c3224b2f385a2
Xavi <57891264+Xavi-PL@users.noreply.github.com>
2021-04-14 20:06:17 +0200

    fixeados upload ficheros

--- www/js/init.js
+++ www/js/init.js
@@ -1,4 +1,6 @@
 var totalReq=0;
+var arrayUF = [];
+
 $.ajax({
     method: "GET",
     // url: "http://localhost:5000/api/auth",                      // [DEBUG] - Para pruebas LOCALHOST
@@ -38,11 +40,11 @@ $.ajax({
   var semaforo1 = document.getElementById("semaforo1");
   var semaforo2 = document.getElementById("semaforo2");
   
+
   function VRed(){    
     semaforo1.style.backgroundColor="#D50000";
     document.getElementById("tsemaforo").innerHTML = "Datos incompletos";
     semaforo2.style.backgroundColor="#D50000";
-  
   }
 
   function VYellow(){
@@ -55,44 +57,48 @@ $.ajax({
     semaforo1.style.backgroundColor="green";
     document.getElementById("tsemaforo").innerHTML = "Datos aceptados";
     semaforo2.style.backgroundColor="green";
+  }  
 
+
+
+  function fileStr(file){
+    const toBase64 = new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => {
+        console.log(err)
+        reject(error);
+      }
+    });
+    
+    return toBase64
   }
 
-  
-  
-  function subirFicheros(){
-    var comprobar =true;
+  async function subirFicheros(){
     for(i=0; i<totalReq; i++ ){
-      if ($("#file"+i).val().length === 0){
-        comprobar =false;
-      }
-    }
-    if (comprobar){
-      for(i=0; i<totalReq; i++ ){
-        
-        var dato_archivo = $("#file"+i).prop("files")[0];
-        var form_data = new FormData();
-        form_data.append("file", dato_archivo);
-        
+      if ($("#file"+i).val().length !== 0){
+        var myFile = $('#file'+i)[0].files[0];
+        var file64 = await fileStr(myFile);
+        var label = $('#file'+i).prop('labels');
+
         $.ajax({
           method: "POST",
-          // url: "http://localhost:5000/alum/login",               // [DEBUG] - Para pruebas LOCALHOST
-          url: "https://g3matriculesapp.herokuapp.com/api/reqPerfils/uploadReq",  // [DEBUG] - Para pruebas con HERKOU
-          datatype: "text",
-          processData: false,
-        
-          data: form_data,
+          url: "http://localhost:5000/api/uploadReq",                      // [DEBUG] - Para pruebas LOCALHOST
+          datatype: "json",
+          data: ({
+            token: localStorage.getItem("TOKEN"),
+            reqName: label[0].innerText,
+            file: file64
+          }),
           success: function(result){
-            alert(result);
+            console.log(result.message)
           },
-         
           error: function(result){
-            alert('Error: '+ result)
+            alert("Error...")
           }
         });
       }
-    }else {
-      alert('Error: datos incompletos')
     }
   }
 
@@ -105,13 +111,51 @@ $.ajax({
       document.getElementById("dPorValidar").onclick = VYellow;
       document.getElementById("dAceptados").onclick = VGreen;
       document.getElementById("cPerf").onclick = subirFicheros;
+      document.getElementById("bGuardar").onclick = guardarUF;
       obtenerCiclo();
       datosP();
       modulos();
-      
-
-      
+       
   }
+
+
+
+  function guardarUF(){
+
+    var arrayCheckbox = [];
+    for(xi = 0; xi < copyResult.data.moduls.length; xi++){
+      for(xj = 0; xj < copyResult.data.moduls[xi].unitatsFormatives.length; xj++){
+        var checkbox = document.getElementById("chk_uf-"+xi+"."+xj);
+        if(checkbox.checked){
+          arrayCheckbox.push("1")
+        }else{
+          arrayCheckbox.push("0")
+        }
+      }
+    }
+
+    arrayUF = arrayCheckbox;
+    $.ajax({
+      method: "PATCH",
+      url: "http://localhost:5000/api/alumnes/updateUF",                      // [DEBUG] - Para pruebas LOCALHOST
+      // url: "https://g3matriculesapp.herokuapp.com/api/auth",          // [DEBUG] - Para pruebas con HERKOU
+      datatype: "json",
+      data: ({
+        token : localStorage.getItem("TOKEN"),
+        data: JSON.stringify(arrayUF)
+      }),
+      success: function(result){
+        alert(result.message)
+      },
+      error: function(result){
+        alert("No tienes acceso")
+        window.location = 'index.html'   
+      }
+  });
+
+  }
+
+
 //  $("#curso1").append('<button class="accordion"><form action="#"><p><label class="black-text"> <input type="checkbox" /> <span>M01</span> </label> </p> </form> </button>')
   function modulos(){
     
@@ -176,7 +220,6 @@ function datosP(){
       
 
       for(i=0; i<result.data.length;i++){
-        console.log("hola "+ result.data.length);
         var perfil = JSON.stringify(result.data[i].nom);
         var codigos = JSON.stringify(result.data[i]._id);
         
@@ -206,17 +249,6 @@ function datosP(){
 });
 }
 
-
-
-//   Sistema de acordeon con botones
-
-function marcarTodo(i, result){
-  console.log("indice= "+i)
-  for(j=0; j<result.data.moduls[i].unitatsFormatives.length; j++){
-    $('#uf'+i+'.'+j).prop('checked', true);
-  }
-}
-
 function obtenerCiclo(){
   $.ajax({
     method: "POST",
@@ -227,43 +259,57 @@ function obtenerCiclo(){
     }),
     success: function(result){
       
+      copyResult = result;
       $("#nomCicle").text(result.data.nom);
       
-      for(i=0; i<result.data.moduls.length;i++){ //  <button id="'+result.data.moduls[i].codiModul+'" > '+result.data.moduls[i].nomModul+' </button>
-        $("#modulos").append('<button class="accordion"> <form action="#"> <p> <a  id="selectAll'+i+'" class="waves-effect waves-light  btn">'+result.data.moduls[i].nomModul+'</a> </p> </form> </button> <div class="panel" id="modul'+i+'"> </div>');
-       
-
-        for(j=0; j<result.data.moduls[i].unitatsFormatives.length; j++){
-          
-       
-          $('#modul'+i).append('<button class="accordion"> <form action="#"> <p> <label class="black-text"> <input id="uf'+i+'.'+j+'" type="checkbox" /> <span>'+result.data.moduls[i].unitatsFormatives[j].nomUnitatFormativa+'</span> </label> </p> </form> </button> <div class="panel" id="uf'+i+'.'+j+'"> </div>')
+      for(i=0; i<result.data.moduls.length;i++){ 
+        $("#modulos").append('<button class="accordion"> <form action="#"> <p> <label class="black-text"> <input id="chk_modul-'+i+'" type="checkbox" /> <span>'+result.data.moduls[i].nomModul+'</span> </label> </p> </form> </button> <div class="panel" id="modul'+i+'"> </div>');
+        
+        countUF = result.data.moduls[i].unitatsFormatives.length;
+        for(j=0; j< countUF ; j++){
+          $('#modul'+i).append('<button class="accordion"> <form action="#"> <p> <label class="black-text"> <input id="chk_uf-'+i+'.'+j+'" type="checkbox" /> <span>'+result.data.moduls[i].unitatsFormatives[j].nomUnitatFormativa+'</span> </label> </p> </form> </button> <div class="panel" id="uf'+i+'.'+j+'"> </div>')
         }
-        document.getElementById("selectAll"+i).onclick = marcarTodo(i, result)
-       // $('#selectAll'+i).click(marcarTodo(i, result));
+        
+        
+        $("#chk_modul-"+i).click({result: result}, function(){
+          var pos = jQuery(this).attr("id");
+          var modulCheckbox = document.getElementById(pos);
+          pos = pos.split("-")[1]
+          var countUF = result.data.moduls[pos].unitatsFormatives.length;
+
+          if(modulCheckbox.checked == true){
+            for(y=0; y<countUF; y++){
+              document.getElementById("chk_uf-"+pos+"."+y).checked = true;
+            }
+          }else{
+            for(y=0; y<countUF; y++){
+              document.getElementById("chk_uf-"+pos+"."+y).checked = false;
+            }
+          }
+
           
-       
-     
+        })
+
       }
 
       var acc = document.getElementsByClassName("accordion");
-      var i;
+      var x;
 
-      for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
-      this.classList.toggle("active");
+      for (x = 0; x < acc.length; x++) {
+        acc[x].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
 
-      /* Toggle between hiding and showing the active panel */
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-      
-    });
-}
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
 
     },
     error: function(result){
